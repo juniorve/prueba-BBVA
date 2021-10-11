62a7f96e98b407b2e63fdb7200b0ce2da7cc3d19
Jofré Valenzuela <jofre.valenzuela@irdigital.pe>
2021-10-10 19:55:05 -0500

    showing the pokemon detail

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 
-async function getList() 
-{
-  let response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
-  let data = await response.json()
-  console.log(response);
-  return data;
+async function getList() {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
+    let data = await response.json()
+    return data;
 }
 
+async function getDetail(url) {
+    let response = await fetch(`${url}`);
+    let data = await response.json()
+    return data;
+}
 
-async function getDetail(value) 
-{
-  let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${value}`);
-  let data = await response.json()
-  console.log(response);
-  return data;
+function showDetail(url) {
+    getDetail(url).then(res => {
+        console.log(res);
+        document.getElementById("name").innerHTML = res.name;
+        document.getElementById("weight").innerHTML = res.weight;
+        document.getElementById("height").innerHTML = res.height;
+    });
 }
